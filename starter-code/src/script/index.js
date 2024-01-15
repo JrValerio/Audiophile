1441814e6cb5dd0c67ebac0d17a9d386b9c52ccb
Amaro V S Júnior <amarovsjr81@gmail.com>
2024-01-15 17:47:36 -0300

    :rocket: "feat: Implementando Funções para Inserção Dinâmica do Footer"

--- starter-code/src/script/index.js
+++ starter-code/src/script/index.js
@@ -1,3 +1,4 @@
+// HTML da Seção "About"
 const aboutHTML = `
   <section id="about" class="about__container">
     <div class="about__content">
@@ -16,10 +17,94 @@ const aboutHTML = `
     </div>        
   </section>
 `;
+
+// HTML do "Footer"
+const footerHTML = `
+<footer class="footer">
+  <div class="footer__content">
+    <div class="footer__line"></div>
+    <div class="footer__Social">
+      <div class="footer__brand">
+        <img
+          src="./starter-code/assets/shared/desktop/logo.svg"
+          alt="Logo audiophile"
+        />
+      </div>
+      <div class="footer__social-media">
+        <nav class="footer__navigation">
+          <ul class="footer__nav-list">
+            <li><a href="/home">Home</a></li>
+            <li><a href="/headphones">Headphones</a></li>
+            <li><a href="/speakers">Speakers</a></li>
+            <li><a href="/earphones">Earphones</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div class="footer__description">
+      <p class="footer__text">
+        Audiophile is an all in one stop to fulfill your audio needs. We're
+        a small team of music lovers and sound specialists who are devoted
+        to helping you get the most out of personal audio. Come and visit
+        our demo facility - we’re open 7 days a week.
+      </p>
+      <ul class="footer__social-icons">
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            aria-label="Facebook"
+            class="footer__icon"
+          >
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            aria-label="Twitter"
+            class="footer__icon"
+          >
+            <i class="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            aria-label="Instagram"
+            class="footer__icon"
+          >
+            <i class="fab fa-instagram"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="footer__copyright">
+      <p>Copyright 2021. All Rights Reserved</p>
+    </div>
+  </div>
+</footer>
+`;
+
+//Funão para a seção "About"
 function insertAboutSection() {
   const aboutContainer = document.getElementById("about-container");
   if (aboutContainer) {
     aboutContainer.innerHTML = aboutHTML;
   }
 }
-document.addEventListener("DOMContentLoaded", insertAboutSection);
+
+//Função para o "Footer"
+function insertFooter() {
+  const footerContainer = document.getElementById("footer-container");
+  if (footerContainer) {
+      footerContainer.innerHTML = footerHTML;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  insertAboutSection();
+  insertFooter();
+});
